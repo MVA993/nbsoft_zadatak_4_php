e93bd09028cbde57d52bf9aa64115503c4709852
MVA993 <87016379+MVA993@users.noreply.github.com>
2021-08-21 18:38:31 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+function listFolderFiles($dir){
+    $flds = scandir($dir);
+
+    unset($flds[array_search('.', $flds, true)]);
+    unset($flds[array_search('..', $flds, true)]);
+
+    // prevent empty ordered elements
+    if (count($flds) < 1){
+        return;
+    }
+
+    echo '<ul>';
+    foreach($flds as $fld){
+        $path = $dir.'/'.$fld;
+        if(!is_dir($path)){
+            echo "  <li>
+                    <a href='$path' target='_blank'>".$fld."</a>
+                    </li>";
+        }else{
+            echo "  <li>
+                    ".$fld."
+                    </li>";
+            listFolderFiles($path);
+        }
+        }
+    echo '</ul>';
+}
+
+listFolderFiles('./folder');
+?>
